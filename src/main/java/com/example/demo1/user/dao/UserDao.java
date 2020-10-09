278eccc780640171ed788654acd5fb6fe4ab6959
1176100802 <1176100802@qq.com>
2020-10-09 08:34:08 +0800

    first

---
+++ src/main/java/com/example/demo1/user/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.example.demo1.user.dao;
+
+import com.example.demo1.user.entity.UserEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.*;
+
+@Mapper
+public interface UserDao {
+
+    List<UserEntity> getAll(@Param("content") Map map);
+
+    void delById(Integer id);
+
+    public void save(UserEntity userEntity);
+
+}
